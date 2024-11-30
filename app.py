68f862a9b80500728f2ce0cd3330164701394698
UdayKarthik2003 <104999797+UdayKarthik2003@users.noreply.github.com>
2024-11-30 18:06:59 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import tensorflow as tf
 from flask import Flask, request, render_template, jsonify
 from tensorflow.keras.models import load_model
